780d0337ae4312b9b1be1a12560bdf86dd9f283c
Edun Yusuf <yusufedun19@gmail.com>
2024-10-17 22:19:17 +0100

    refactor(navbar): improve layout and styling for better responsiveness and consistency

--- src/components/Layout/header/navbar.tsx
+++ src/components/Layout/header/navbar.tsx
@@ -1,4 +1,3 @@
-
 import { customNavbarTheme } from "@/utils/FlowBiteCustomThemes";
 import NavBarLinks from "@/components/Ui/navbarLinks";
 import { Button, Navbar } from "flowbite-react";
@@ -8,23 +7,25 @@ import AdSlider from "./adSlider";
 export default function Header() {
   return (
     <>
-      <AdSlider/>
+      <AdSlider />
       <Navbar fluid theme={customNavbarTheme} className="bg-white text-black-500">
         <Navbar.Brand href="/">
           <img src="assets/images/landingPage/favicon.png" className="mr-3 h-8 sm:h-11" alt="Predict.if Logo" />
-          <span className="self-center whitespace-nowrap text-3xl font-bold text-[#002072] ">Predict.if</span>
+          <span className="self-center whitespace-nowrap text-3xl font-bold text-[#002072]">Predict.if</span>
         </Navbar.Brand>
         <div className="flex md:order-2">
-          <Link to="/auth/signin"><Button color="dark">Login / Register</Button></Link>
+          <Link to="/auth/signin" className="hidden md:block">
+            <Button color="dark">Login / Register</Button>
+          </Link>
           <Navbar.Toggle />
         </div>
         <Navbar.Collapse>
           <NavBarLinks />
+          <Link to="/auth/signin" className="block md:hidden mt-4">
+            <Button color="dark" className="w-full">Login / Register</Button>
+          </Link>
         </Navbar.Collapse>
       </Navbar>
     </>
   );
 }
-  
-
-
--- src/utils/FlowBiteCustomThemes.tsx
+++ src/utils/FlowBiteCustomThemes.tsx
@@ -2,12 +2,12 @@ import { CustomFlowbiteTheme } from "flowbite-react";
 
 export const customNavbarTheme: CustomFlowbiteTheme["navbar"] = {
   root: {
-    base: "bg-white px-2 py-2.5 sm:px-4 sticky top-0 z-50",
+    base: "bg-white px-2 py-2.5 dark:border-gray-700 dark:bg-gray-800 sm:px-4 sticky top-0 z-50",
   },
   link: {
     active: {
-      on: "md:text-blue-700 md:dark:text-blue-500 md:bg-transparent bg-blue-500 dark:text-white text-black-500 md:rounded-none rounded border-b-2 border-blue-500",
-      off: "text-black-500",
+      on: "md:text-blue-700 md:dark:text-blue-300 md:bg-transparent bg-blue-500 dark:text-white text-blue-700 md:rounded-none rounded border-b-2 border-blue-500",
+      off: "text-black hover:text-blue-500 dark:text-white",
     },
   },
 };
